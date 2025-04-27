bc5ba414a560a4fd0da1816cb4594f975a464d19
Roger Fan <sweric2u@gmail.com>
2025-04-27 13:31:57 +0800

    backend communication setting

--- frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ frontend/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "crowddashboard": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "hosting": {
     "S3AndCloudFront": {
       "CloudFrontDistributionID": "string",
